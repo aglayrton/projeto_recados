ab871b461651291c88c5768a5a5538b5de4d8293
aglayrton <aglayrtonjuliao@gmail.com>
2023-02-27 20:22:44 -0300

    Rota Home criada

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { GlobalStyle } from "../config";
+import Home from "../pages/Home/Home";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
 
@@ -12,6 +13,7 @@ const AppRoutes = () => {
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
+          <Route path='/home' element={<Home />} />
           <Route path='*' element={<h1>Not found</h1>} />
         </Routes>
       </BrowserRouter>
