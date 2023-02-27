1fb7544752c1bced378183b2941029fd30b1af2d
aglayrton <aglayrtonjuliao@gmail.com>
2023-02-27 10:40:56 -0300

    Componente de input personalizado

---
+++ src/components/InputDefault/index.tsx
@@ -0,0 +1,40 @@
+import { TextField } from "@mui/material";
+import React from "react";
+
+interface InputDefaultProps {
+  name: Name; //ante era string
+  type: string;
+  label: string;
+  value: string;
+  handleChange: (value: string, key: Name) => void; //(funcao que toda vez que o textfield modifica) o tipo dela pode ser Function
+  //colocando o color depois que fizer o useEffect
+  color: "error" | "secondary";
+}
+
+export type Name = "name" | "email" | "password" | "repassword";
+
+//se eu fosse receber a funcao abaixo eu faria handleChange: ()=>React.ReactNode;
+const InputDefault: React.FC<InputDefaultProps> = ({
+  name,
+  type,
+  label,
+  value,
+  color,
+  handleChange,
+}) => {
+  return (
+    <React.Fragment>
+      <TextField
+        name={name}
+        label={label}
+        value={value}
+        onChange={(e) => handleChange(e.target.value, name)}
+        type={type}
+        variant='outlined'
+        color={color}
+      />
+    </React.Fragment>
+  );
+};
+
+export default InputDefault;
