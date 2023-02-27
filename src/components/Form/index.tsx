6daff4fad83ec4533c85d932298e0808ac3b7cae
aglayrton <aglayrtonjuliao@gmail.com>
2023-02-27 10:39:45 -0300

    Criado a página de cadastro e de conta

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
-import { Stack, TextField, Button, Typography } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { Stack, Button, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import InputDefault, { Name } from "../InputDefault";
 
 interface FormProps {
   mode: "login" | "signup";
@@ -8,7 +9,13 @@ interface FormProps {
 
 const Form = ({ mode }: FormProps) => {
   const navigate = useNavigate();
-  const [value, setValue] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repassword, setRepassword] = useState("");
+
+  const [errorEmail, setErrorEmail] = useState(false);
+  const [errorPassword, setErrorPassword] = useState(false);
+
   const outraPagina = () => {
     if (mode === "login") {
       navigate("/signup");
@@ -16,17 +23,89 @@ const Form = ({ mode }: FormProps) => {
       navigate("/");
     }
   };
+
+  //eu pego o valor digitado e pego o nome do campo
+  //do tipo Name so pode receber os valores que estão nesse type, dai o massa é porque vai mudar o estado somente daquele cara que foi digitado
+  const mudarInput = (value: string, key: Name) => {
+    //faço o switch pra nao pegar todos diretamente
+    switch (key) {
+      case "email":
+        setEmail(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+      case "repassword":
+        setRepassword(value);
+        break;
+      default:
+    }
+  };
+
+  useEffect(() => {
+    if (email.length < 3) {
+      //so para mudar a cor
+      setErrorEmail(true);
+    } else {
+      setErrorEmail(false);
+    }
+
+    if (mode === "signup") {
+      if (
+        !password ||
+        !repassword ||
+        password.length < 3 ||
+        password !== repassword
+      ) {
+        //so para mudar a cor
+        setErrorPassword(true);
+      } else {
+        setErrorPassword(false);
+      }
+    }
+
+    if (mode === "login") {
+      if (
+        !password
+      ) {
+        //so para mudar a cor
+        setErrorPassword(true);
+      } else {
+        setErrorPassword(false);
+      }
+    }
+  }, [email, password, repassword, mode]);
+
+  const createAccount = () => {
+    const newUser = {
+      email,
+      password,
+      recados: [],
+    };
+    console.log(newUser);
+  };
+
   return (
     <React.Fragment>
+      {" "}
       <Stack spacing={2}>
         {mode == "login" && (
           <>
-            <TextField id='outlined-password-input' label='Email' type='text' />
-            <TextField
-              id='outlined-password-input'
+            <InputDefault
+              name='email'
+              label='Email'
+              type='email'
+              value={email}
+              color={errorEmail ? "error" : "secondary"}
+              handleChange={mudarInput}
+            />
+            <InputDefault
+              name='password'
               label='Password'
               type='password'
-              autoComplete='current-password'
+              value={password}
+              color={errorPassword ? "error" : "secondary"}
+              handleChange={mudarInput}
             />
             <Button variant='contained' color='success'>
               Logar
@@ -46,30 +125,41 @@ const Form = ({ mode }: FormProps) => {
 
         {mode == "signup" && (
           <>
-            <TextField
-              id='outlined-password-input'
+            <InputDefault
+              name='email'
               label='Email'
-              type='text'
-              value={value}
-              onChange={(e) => setValue(e.target.value)}
-              error={value.length > 1 && value.length <8 ? true : false}
-              helperText={!value ? "Required" : ""}
+              type='email'
+              value={email}
+              color={errorEmail ? "error" : "secondary"}
+              handleChange={mudarInput}
             />
-            <TextField
-              id='outlined-password-input'
+            <InputDefault
+              name='password'
               label='Password'
               type='password'
-              autoComplete='current-password'
+              value={password}
+              color={errorPassword ? "error" : "secondary"}
+              handleChange={mudarInput}
             />
-            <TextField
-              id='outlined-password-input'
-              label='Confirm Password'
+            <InputDefault
+              name='repassword'
+              label='Password'
               type='password'
-              autoComplete='current-password'
+              value={repassword}
+              color={errorPassword ? "error" : "secondary"}
+              handleChange={mudarInput}
             />
             <Button
               variant='contained'
               color='success'
+              disabled={errorEmail || errorPassword}
+              onClick={createAccount}
+            >
+              Criar Conta
+            </Button>
+            <Button
+              variant='contained'
+              color='warning'
               onClick={() => outraPagina()}
             >
               Voltar
