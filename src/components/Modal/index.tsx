a2da0ffd00d13d5fbaf8cb61b041389be9ddb618
aglayrton <aglayrtonjuliao@gmail.com>
2023-03-14 00:25:43 -0300

    Modal com opção de editar

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,20 +1,57 @@
-import React from "react";
+import {  useState } from "react";
 import Button from "@mui/material/Button";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
-import { Dialog } from "@mui/material";
-import { Recado } from "../../types/recadoType";
+import { Dialog, TextField } from "@mui/material";
+import { User } from "../../types/userType";
 
 interface ModalProps {
   open: boolean;
   handleClose: () => void;
-  conteudo: Recado;
+  user: User;
+  setUser: React.Dispatch<React.SetStateAction<User | null>>; //passar o set do userLogged, ou seja, vamos passar a funcao de set
   indice: number;
+  mode: string;
+  description?: string;
+  detail?: string;
 }
 
-function Modal({ open, handleClose, conteudo, indice }: ModalProps) {
+function Modal({
+  open,
+  handleClose,
+  indice,
+  user,
+  setUser,
+  mode,
+  description,
+  detail,
+}: ModalProps) {
+  const [descriptionUpdate, setDescriptionUpdate] = useState<string>("");
+  const [detailUpdate, setDetailUpdate] = useState("");
+
+  const handleConfirm = () => {
+    const temp = [...user.recados]; //pego tudo o que tem dentro do vetor
+    temp.splice(indice, 1); //apago de la
+    setUser({ ...user, recados: temp }); //coloco o no vetor o vetor recados modificado
+    handleClose();
+  };
+
+  const handleUpdate = () => {
+    const temp = [...user.recados]; // criar uma cópia do array de recados
+    const updatedRecado = { ...temp[indice] }; // criar uma cópia do recado que será atualizado
+    if (descriptionUpdate) {
+      updatedRecado.description = descriptionUpdate;
+    }
+    if (detailUpdate) {
+      updatedRecado.detail = detailUpdate;
+    }
+    temp[indice] = updatedRecado; // atualizar a cópia do recado no array de recados
+    setUser({ ...user, recados: temp }); // atualizar o usuário com o novo array de recados
+    handleClose();
+  };
+
   return (
     <Dialog
       open={open}
@@ -22,22 +59,78 @@ function Modal({ open, handleClose, conteudo, indice }: ModalProps) {
       aria-labelledby='alert-dialog-title'
       aria-describedby='alert-dialog-description'
     >
-      <DialogTitle id='alert-dialog-title'>
-        {`Tem certeza que deseja excluir o recado de ID ${indice}?`}
-      </DialogTitle>
-      <DialogContent>
-        <DialogContentText id='alert-dialog-description'>
-          Ao confirmar esta ação, não poderá ser desfeita.
-        </DialogContentText>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose}>Confirmo</Button>
-        <Button onClick={handleClose} autoFocus>
-          Cancelar
-        </Button>
-      </DialogActions>
+      {mode === "delete" && (
+        <>
+          <DialogTitle id='alert-dialog-title'>
+            {`Tem certeza que deseja excluir o recado de ID ${indice}?`}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id='alert-dialog-description'>
+              Ao confirmar esta ação, não poderá ser desfeita.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleConfirm} color='info'>
+              Confirmo
+            </Button>
+            <Button onClick={handleClose} autoFocus color='warning'>
+              Cancelar
+            </Button>
+          </DialogActions>
+        </>
+      )}
+
+      {mode === "update" && (
+        <div>
+          <DialogTitle id='alert-dialog-title'>
+            {`Os dados do ID ${indice} serão atualizados`}
+          </DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin='dense'
+              id='name'
+              label='Descrição'
+              value={descriptionUpdate}
+              fullWidth
+              variant='standard'
+              onChange={(e) => setDescriptionUpdate(e.target.value)}
+            />
+            <TextField
+              autoFocus
+              margin='dense'
+              id='name'
+              label='Detalhe'
+              value={detailUpdate}
+              fullWidth
+              variant='standard'
+              onChange={(e) => setDetailUpdate(e.target.value)}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleUpdate} color='info'>
+              Confirmo
+            </Button>
+            <Button onClick={handleClose} autoFocus color='warning'>
+              Cancelar
+            </Button>
+          </DialogActions>
+        </div>
+      )}
     </Dialog>
   );
 }
 
 export default Modal;
+
+// const handleConfirm = () => {
+//   if (userLogged) {
+//     setUserLogged(
+//       (prev)=>{
+//         prev?.recados.splice(indice, 1);
+//         return prev
+//       }
+//     )
+//     handleClose();
+//   }
+// };
